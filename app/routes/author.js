a9dfdc55ccac9c78732d9425254f2da61d70dde8
Adam Lukasiewicz <mr.westbee@gmail.com>
2019-01-10 12:35:04 +0100

    [9] Fetching API Data From Ember Routes

--- app/routes/author.js
+++ app/routes/author.js
@@ -2,32 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return [
-      {
-        first: 'J.K.',
-        last: 'Rowling',
-        id: 1
-      },
-      {
-        first: 'Goerge R.R.',
-        last: 'Martin',
-        id: 2
-      },
-      {
-        first: 'Tom',
-        last: 'Clancy',
-        id: 3
-      },
-      {
-        first: 'Stephen',
-        last: 'King',
-        id: 4
-      },
-      {
-        first: 'Tsukasa',
-        last: 'Fushimi',
-        id: 5
-      }
-    ];
+    return fetch('http://localhost:3000/authors')
+      .then(response => response.json());
   }
 });
