1685932e469d7e228214e7aa9d0f129ed5910b11
Adam Lukasiewicz <mr.westbee@gmail.com>
2019-01-11 10:23:51 +0100

    [20] Reset Data

--- app/controllers/author/create.js
+++ app/controllers/author/create.js
@@ -1,6 +1,13 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  reset() {
+    this.setProperties({
+      first: '',
+      last: ''
+    });
+  },
+  
   actions: {
     changeFirst(value) {
       this.set('first', value);
--- app/routes/author/create.js
+++ app/routes/author/create.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  // resetController(controller) {
+  //   this._super(...arguments);
+  //
+  //   // controller.set('first', '');
+  //   // controller.set('last', '');
+  //
+  //   // controller.setProperties({
+  //   //   first: '',
+  //   //   last: ''
+  //   // });
+  //
+  //   controller.reset();
+  // },
+
+  model() {
+    return {
+      first: '',
+      last: ''
+    };
+  }
 });
