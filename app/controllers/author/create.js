8a43d21496a81ffc608f134dda2567f974892699
Adam Lukasiewicz <mr.westbee@gmail.com>
2019-01-11 10:33:16 +0100

    [21] Ember Data Create Record

--- app/controllers/author/create.js
+++ app/controllers/author/create.js
@@ -1,16 +1,26 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  reset() {
-    this.setProperties({
-      first: '',
-      last: ''
-    });
-  },
-  
+  // reset() {
+  //   this.setProperties({
+  //     first: '',
+  //     last: ''
+  //   });
+  // },
+
   actions: {
-    changeFirst(value) {
-      this.set('first', value);
+    // changeFirst(value) {
+    //   this.set('first', value);
+    // }
+    saveAuthor(ev) {
+      ev.preventDefault();
+
+      let author = this.store.createRecord('author', this.model);
+
+      author.save()
+        .then(() => {
+          this.transitionToRoute('author');
+        });
     }
   }
 });
