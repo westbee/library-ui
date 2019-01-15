902f734766166671aef6b7e6e6e5242e0028738b
Adam Lukasiewicz <mr.westbee@gmail.com>
2019-01-15 11:43:36 +0100

    [35] Ember Data Has Many Relationships

--- app/models/author.js
+++ app/models/author.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   first: DS.attr('string'),
-  last: DS.attr('string')
+  last: DS.attr('string'),
+
+  books: DS.hasMany('book')
 });
